2dd0c670d8a1190da1237f24c23b0d414f66c3b2
Tarcio Diniz <tarciodiniz0@gmail.com>
2025-04-11 22:05:54 -0300

    wip: Solution Two Pointer String

---
+++ src/main/java/com/data_struct_lab/examples/TwoPointer/TwoPointer.java
@@ -0,0 +1,24 @@
+package com.data_struct_lab.examples.TwoPointer;
+
+public class TwoPointer {
+
+    public static String reverse(String str) {
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return String.valueOf(arr);
+    }
+
+    public static void main(String[] args) {
+        String input = "Tarcio Elyakin Agra Diniz";
+        System.out.println(TwoPointer.reverse(input));
+    }
+
+}
