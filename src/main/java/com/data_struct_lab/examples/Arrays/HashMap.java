82a8479190c3148c5a9ad698fc63254f3339d794
Tarcio Diniz <tarciodiniz0@gmail.com>
2025-04-14 08:50:56 -0300

    wip: HashMap

---
+++ src/main/java/com/data_struct_lab/examples/Arrays/HashMap.java
@@ -0,0 +1,23 @@
+package com.data_struct_lab.examples.Arrays;
+
+public class HashMap {
+
+    public static Integer firstUniqChar(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+    }
+    
+}
